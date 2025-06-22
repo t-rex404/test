685e67574644cb464cf43659688fde2473c62283
megu <119268055+t-rex404@users.noreply.github.com>
2025-06-22 23:00:01 +0900

    mod file

--- Common.ps1
+++ Common.ps1
@@ -2,8 +2,7 @@ class Common
 {
     Common()
     {
-        /html/body/div[1]/div[4]/div/main/turbo-frame/div/div/div/div/div[1]/react-partial/div/div/div[2]/div[2]/button/span/span[2]
-        /html/body/div[4]/div/div/ul/div/ul/li[2]/a/div/span
+        
     }
 }
 
